06df68f5b208e824fbd4cf227c5660f217392612
Vasily Myazin <vm@konspiredesign.com>
2014-01-17 13:52:44 -0800

    Fixed search input form

--- scripts/app.js
+++ scripts/app.js
@@ -73,7 +73,7 @@ var MainView = Backbone.View.extend({
     }, 0);
 
     // init search input, prevent form submission
-    $('#search').focus().submit(function() { return; });
+    $('#search').focus().parents('form').submit(function(e) { e.preventDefault(); return; });
   },
   bindMapLocationLinks: function() {
     $('.locations-list').find('li:not(.trailer-link-container)').find('a').click(function() {
